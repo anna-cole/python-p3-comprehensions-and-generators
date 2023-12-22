17c5601aa18358ea201de14efe410dffe3f768f6
Anna Raptidou Cole <acrrj123@gmail.com>
2023-12-21 22:44:38 -0600

    Lab done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
+# To return evens, use the remainder: n % 2 == 0
+
 def return_evens(num_list):
-    pass
+    even_list = [num for num in num_list if num % 2 == 0]
+    return even_list
+
+return_evens([0, 1, 3, 5, 7, 8, 9])
+
+# The sentences in sentence_list are represented as strings. To modify each sentence to add an exclamation mark at the end, use a list comprehension to iterate over each sentence in the original list and concatenate an exclamation mark at the end of each sentence. We use the + operator to concatenate.
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [sentence + '!' for sentence in sentence_list]
+    return exclamation_list
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
